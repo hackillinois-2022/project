0db142c7b34a2466ae6031de3c9d16b33bf2703f
Peter Stenger <peter.a.stenger@gmail.com>
2022-02-25 19:56:26 -0600

    confirming tailwind css works in components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
-import './App.css';
-
+import Box from './components/Box';
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Box />
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Box/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Box() {
+  return <div class="border border-red-100 text-lg bg-red-400 text-white"> tailwind tests</div>;
+}
+
+export default Box;
