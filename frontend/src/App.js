a809a7728ee29c1b1fa746959e509684d41466ca
Pete Stenger <pete@stenger.io>
2022-02-25 23:15:55 -0600

    it begins :(

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,48 @@
-import Box from './components/Box';
+import React, { useState } from 'react';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Notifications from './components/Notifications';
+import Predictions from './components/Predictions';
+import Welcome from './components/Welcome';
+import Inventory from './components/Inventory';
+import SignIn from './components/SignIn';
+import Registration from './components/Registration';
+
 function App() {
+  // cheese cuz authentication isn't actually real
+  let [username, setUsername] = useState(null);
+  // how do i modify menu bar based on signed in
+
+  let signedOut = username === null;
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Box />
-    </div>
-  );
+    <Router>
+      <MenuBar username={username} setUserName={setUsername} />
+      <Switch>
+        <Route path="/notifications">
+        {signedOut ? <Redirect to="/" /> : <Notifications />}
+        </Route>
+        <Route path="/register">
+        {signedOut ? <Redirect to="/" /> : <Registration />}
+        </Route>
+        <Route path="/signin">
+        {signedOut ? <Redirect to="/" /> : <SignIn />}
+        </Route>
+        <Route path="/">
+        {signedOut ? <Redirect to="/" /> : <Welcome />}
+        </Route>
+        <Route path="/inventory">
+        {signedOut ? <Redirect to="/" /> : <Inventory />}
+        </Route>
+      </Switch>
+    </Router>
+  )
+
 }
 
 export default App;
---
+++ frontend/src/components/Predictions/index.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react'
+
+function Predictions() {
+
+}
+
+export default Predictions;
