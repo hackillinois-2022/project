07942706b234f1db7352bd4bdd22efa927be3d13
Pete Stenger <pete@stenger.io>
2022-02-27 03:34:30 -0600

    Final Frontend Changes

--- frontend/src/components/MenuBar/index.js
+++ frontend/src/components/MenuBar/index.js
@@ -21,7 +21,7 @@ function MenuBar({ username, setUsername }) {
     return (
         <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
             <div className="flex items-center flex-shrink-0 text-white mr-6">
-            <span className="font-semibold text-xl tracking-tight">Produce Predictor</span>
+            <span className="font-semibold text-xl tracking-tight">AgroGEN</span>
             </div>
             <div className="block lg:hidden">
             <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
--- frontend/src/components/Welcome/index.js
+++ frontend/src/components/Welcome/index.js
@@ -9,8 +9,8 @@ function Welcome({ username }) {
     return (
     <div className="container mx-auto flex flex-col md:flex-row items-center my-12 md:my-24">
 		<div className="flex flex-col w-full lg:w-1/2 justify-center items-start pt-12 pb-24 px-6">
-			<p className="uppercase text-2xl mb-2">Witty Tagline</p>
-			<p className="leading-normal mb-4">Welcome to Produce Predictor. Our team of developers here at Illinois have created a system in order to increase profits for you, the farmer, by predicting crop prices.</p>
+			<p className="font-bold text-3xl mb-2">Maximize Your Profits</p>
+			<p className="leading-normal mb-4">AgroGen is a system for farmers to improve their profits by predicting the future prices of their produce. </p>
 			<Link to="/register" className="bg-transparent hover:bg-black text-black hover:text-white rounded shadow hover:shadow-lg py-2 px-4 border border-black hover:border-transparent">Register</Link>
 		</div>
 		<div className="w-full lg:w-1/2 lg:py-6 text-center">
